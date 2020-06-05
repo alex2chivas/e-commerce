d07502d2a0ef5c2d133081cbe75249c58de17fb1
Alexis Flores <alex2chivas@hotmail.com>
2020-06-05 14:57:43 -0700

    built account information form from sign form as template

--- src/components/account/accountInformation.js
+++ src/components/account/accountInformation.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
+import PageTitle from '../pageTitle';
+import AccountInformationForm from './accountInformationFrom';
+
 const AccountInformation = () => {
+
+    const onSubmit = fields => {
+        console.log(fields)
+    }
+
     return (
-        <div>
-            Account Information
+        <div className='account-information'>
+            <PageTitle className='account-information' title='Account Information'/>
+            <AccountInformationForm onSubmit={onSubmit} className='account-information__form' />
         </div>
     );
 }
