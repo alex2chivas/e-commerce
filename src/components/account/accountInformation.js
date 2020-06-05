baa1a6e07ae4799bc3148066c80af318dcf27e32
Alexis Flores <alex2chivas@hotmail.com>
2020-06-05 16:34:03 -0700

    built account information grid and palced majority of fields on the grid

--- src/components/account/accountInformation.js
+++ src/components/account/accountInformation.js
@@ -4,17 +4,16 @@ import PageTitle from '../pageTitle';
 import AccountInformationForm from './accountInformationFrom';
 
 const AccountInformation = () => {
+	const onSubmit = fields => {
+		console.log(fields);
+	};
 
-    const onSubmit = fields => {
-        console.log(fields)
-    }
-
-    return (
-        <div className='account-information'>
-            <PageTitle className='account-information' title='Account Information'/>
-            <AccountInformationForm onSubmit={onSubmit} className='account-information__form' />
-        </div>
-    );
-}
+	return (
+		<div className='account-information'>
+			<PageTitle className='account-information' title='Account Information' />
+			<AccountInformationForm onSubmit={onSubmit} className='account-information__form' />
+		</div>
+	);
+};
 
 export default AccountInformation;
--- src/components/account/accountInformationFrom.js
+++ src/components/account/accountInformationFrom.js
@@ -5,39 +5,81 @@ import history from '../../history';
 import { FormInput, FormButton } from '../formFields';
 
 const AccountInformationForm = props => {
-    const { className, handleSubmit } = props;
+	const { className, handleSubmit } = props;
 
-    return (
-        <form onSubmit={handleSubmit} className={`${className} sign-in-form`}>
-            <Field
-                className='sign-in-form__email'
-                type='email'
-                title='Email'
-                placeholder='Email'
-                name='email'
-                component={FormInput}
-            />
-            <Field
-                className='sign-in-form__password'
-                type='password'
-                title='Password'
-                placeholder='Password'
-                name='password'
-                component={FormInput}
-            />
-            <div className='sign-in-form__line' />
-            <Field
-                onClick={() => history.push('/account')}
-                className='sign-in-form__login'
-                type='submit'
-                title='Login'
-                name='login'
-                component={FormButton}
-            />
-        </form>
-    );
+	return (
+		<form onSubmit={handleSubmit} className={`${className} account-information-form`}>
+			<Field
+				className='account-information-form__name'
+				type='name'
+				title='Name'
+				placeholder='Name'
+				name='name'
+				component={FormInput}
+			/>
+			<Field
+				className='account-information-form__email'
+				type='email'
+				title='Email'
+				placeholder='Email'
+				name='email'
+				component={FormInput}
+			/>
+
+			<Field
+				className='account-information-form__street-address'
+				type='address'
+				title='Street Address'
+				placeholder='Street Address'
+				name='address'
+				component={FormInput}
+			/>
+			<Field
+				className='account-information-form__city'
+				type='city'
+				title='City'
+				placeholder='City'
+				name='city'
+				component={FormInput}
+			/>
+
+			<Field
+				className='account-information-form__state'
+				type='state'
+				title='State'
+				placeholder='State'
+				name='state'
+				component={FormInput}
+			/>
+			<Field
+				className='account-information-form__zipcode'
+				type='zipeCode'
+				title='ZipeCode'
+				placeholder='ZipeCode'
+				name='zipeCode'
+				component={FormInput}
+			/>
+
+			{/* <Field
+				className='account-information-form__password'
+				type='password'
+				title='Password'
+				placeholder='Password'
+				name='password'
+				component={FormInput}
+			/>
+			<Field
+				onClick={() => history.push('/account')}
+				className='account-information-form__login'
+				type='submit'
+				title='Login'
+				name='login'
+				component={FormButton}
+			/> */}
+		</form>
+	);
 };
 
 export default reduxForm({
-    form: 'AccountInformationForm'
+	form: 'AccountInformationForm'
 })(AccountInformationForm);
